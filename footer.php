d1f5017d162aa20d458cb41eacca653224e40f6a
Juan Esquivias <juanesquiviasotazu@gmail.com>
2019-07-18 16:35:50 -0500

    created the footer

---
+++ footer.php
@@ -0,0 +1,26 @@
+				<footer class="footer text-center">
+					Todos los derechos reservados. Desarrollado por
+					<a href="http://laeliteweb.com" target="_blank">La Élite Web</a>.
+				</footer>
+    	</div>
+    </div>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/jquery/dist/jquery.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/popper.js/dist/umd/popper.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/extra-libs/sparkline/sparkline.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/waves.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/sidebarmenu.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/custom.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/chartist/dist/chartist.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/pages/dashboards/dashboard1.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.basictable.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.dataTables.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/dataTables.bootstrap.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.mask.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.richtext.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap3-typeahead.min.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/init.js"></script>
+</body>
+
+</html>
