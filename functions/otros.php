db1ce032c0cbc6aaf29cceeaae672d1fdfee5423
Juan Esquivias <juanesquiviasotazu@gmail.com>
2019-07-20 15:28:23 -0500

    continue with create of otros.php

--- functions/otros.php
+++ functions/otros.php
@@ -28,11 +28,11 @@ function _remover_version_javascript( $src) {
  * Function for short large text
  */
 function cortarTexto( $cadena, $limite, $alto, $comodin = '...' ) {
-	if ( strlen($cadena) < $limite ) {
+if ( strlen($cadena) < $limite ) {
 		return $cadena;
 	}
 
-	if ( false !== ($punto = strpos( $cadena, $alto, $limite ) ) {
+	if ( false !== ($punto = strpos( $cadena, $alto, $limite ) ) ) {
 		if ( $punto < strlen($cadena) -1 ) {
 			$cadena = substr( $cadena, 0, $punto ).$comodin;
 		}
@@ -43,9 +43,84 @@ function cortarTexto( $cadena, $limite, $alto, $comodin = '...' ) {
 /*
  * Shortcodes in WordPress
 */
-
 add_filter('widget_text', 'shortcode_anautop');
 add_filter('widget_text', 'do_shortcode');
+
+/*
+ * Function for attacks with XSS via FORM
+*/
+function antiXSS( $input ) {
+	$value = htmlspecialchars( rawurldecode( trim( $input ) ), ENT_QUOTES, 'UTF-8' );
+	return $value;
+}
+
+/*
+ * Function of control for user that view privates pages and not login session with XSS via FORM
+*/
+function sesion_control() {
+	if ( !is_user_logged_in() ) {
+		wp_redirect( home_url('/') );
+		exit;
+	}
+}
+
+/*
+ * Function of control for user that view privates pages and start login session with XSS via FORM
+*/
+function login_session_control() {
+	if ( is_user_logged_in() ) {
+		wp_redirect( home_url('/otra') );
+	}
+}
+
+/*
+ * Function for USER
+*/
+function obtener_tipo_usuario($role) {
+	$args = array(
+		'role' => '$role'
+	);
+
+	$user = get_users($args);
+	$usuarios = array();
+
+	foreach( $users as $user ) {
+		$usuarios = array(
+			'id' => $user->ID,
+			'nombre' => $user->data->display_name,
+			'email' => $user->data->user_email
+		);
+	}
+}
+
+function get_all_users() {
+	$args = array();
+	$users = get_users($args);
+	$usuarios = array();
+
+	foreach ( $users as $user ) {
+		if ( $user->roles[0] == 'administrator' ) {
+			$rol = 'Administrador';
+		} else if( $user->role[0] == 'secretaria'  ) {
+			$rol = 'Secretaria';
+		} else if( $user->role[0] == 'contributor'  ) {
+			$rol = 'Colaborador';
+		} else {
+			$rol = 'Otro tipo de usuario';
+		}
+
+		$nombre = get_user_meta( $user->ID, 'first_name', true );
+		$apellido = get_user_meta( $user->ID, 'last_name', true );
+
+		$usuario() = array(
+			'id' => $user->ID,
+			'nombre' => $nombre.''.$apellido,
+			'email' => $user->data->user_email,
+			'rol' => $rol
+		);
+	}
+
+	return $usuarios;
 
 
 
