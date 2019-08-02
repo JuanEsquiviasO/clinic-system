e6484d50b5eff57f8b8611053ca565301b1c688e
Juan Esquivias <juanesquiviasotazu@gmail.com>
2019-08-02 12:33:37 -0500

    fixed otros.php with the hook for eliminate JS Version

--- functions/otros.php
+++ functions/otros.php
@@ -3,7 +3,7 @@
 Function Principals
 */
 add_action( 'wp_head', 'set_ajaxurl', 1 );
-function set__ajaxurl() {
+function set_ajaxurl() {
 	echo '<script type="text/javascript"> var ajaxurl = "'.admin_url('admin-ajax.php').'"</script>';
 }
 
@@ -16,8 +16,8 @@ add_filter('the_generator', '__return_false');
 /*
 Eliminated JS version
 */
-add_filter('script_loader_src', 'remover_version_javascript', 15, 1);
-add_filter('style_loader_src', 'remover_version_javascript', 15, 1);
+add_filter('script_loader_src', '_remover_version_javascript', 15, 1);
+add_filter('style_loader_src', '_remover_version_javascript', 15, 1);
 
 function _remover_version_javascript( $src) {
 	$parts = explode('?', $src);
